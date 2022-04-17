d1bd1e9dc8515a2c600789cff442743907f88b3c
Jalal Mostafa <jalalmostafa22@gmail.com>
2022-04-18 01:57:35 +0200

    printf frames

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # eBPF-DAQ
 
+## Test Environment
+
+Run `./testenv.sh` with no parameter to get a list of available commands, or
+run `./testenv.sh --help` to get the full help listing with all options. The
+script can maintain several environments active at the same time, and you
+can switch between them using the `--name` option.
+
+If you don't specify a name, the most recently used environment will be
+used. If you don't specify a name when setting up a new environment, a
+random name will be generated for you.
+
+Examples:
+
+Setup new environment named "test": `./testenv.sh setup --name=test`
+
+Create a shell alias for easy use of script from anywhere: `eval $(./testenv.sh alias)`
+
+See the currently active environment, and a list of all active environment
+names (with alias defined as above): `t status`
+
+Enter the currently active environment: `t enter`
+
+Execute a command inside the environment: `t exec -- ip a`
+
+Teardown the environment: `t teardown`
+
+
 ## Build
 
-`apt install binutils-dev`
+### Ubuntu
+
+```bash
+apt install clang llvm libelf-dev libpcap-dev gcc-multilib build-essential linux-tools-common linux-tools-generic linux-headers-$(uname -r)
+make
+```
--- src/daq.c
+++ src/daq.c
@@ -1,58 +1,204 @@
 // SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
-#include "daq.skel.h"
+#include <bpf/bpf.h>
 #include <bpf/libbpf.h>
+#include <errno.h>
+#include <linux/if_ether.h>
+#include <linux/if_link.h>
+#include <net/if.h>
+#include <poll.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/resource.h>
 #include <unistd.h>
+#include <xdp/libxdp.h>
+#include <xdp/xsk.h>
 
-static int libbpf_print_fn(enum libbpf_print_level level, const char* format, va_list args)
+#define UMEM_LEN 1024
+#define FRAME_SIZE XSK_UMEM__DEFAULT_FRAME_SIZE
+#define FRAME_INVALID -1
+
+typedef unsigned int queue_id;
+
+typedef struct {
+    struct xsk_umem* umem;
+    struct xsk_ring_prod fill_ring;
+    struct xsk_ring_cons comp_ring;
+    void* buffer;
+} umem_info;
+
+typedef struct {
+    char* ifname;
+    queue_id qid;
+} net_info;
+
+typedef struct {
+    struct xsk_socket* socket;
+    struct xsk_ring_prod tx;
+    struct xsk_ring_cons rx;
+    __u32 libbpf_flags;
+    __u32 xdp_flags;
+    __u16 bind_flags;
+} xsk_info;
+
+__u32 break_flag = 0;
+
+static void* umem_buffer_create()
 {
-    return vfprintf(stderr, format, args);
+    // use huge pages?
+    return calloc(1, UMEM_LEN);
+}
+
+static umem_info* umem_info_create()
+{
+    umem_info* info = (umem_info*)calloc(1, sizeof(umem_info));
+
+    if (info == NULL) {
+        return NULL;
+    }
+
+    info->buffer = umem_buffer_create();
+    info->umem = NULL;
+
+    return info;
+}
+
+static void umem_info_free(umem_info* info)
+{
+    if (info != NULL) {
+        free(info->buffer);
+        xsk_umem__delete(info->umem);
+        if (info->umem != NULL) {
+            free(info->umem);
+        }
+    }
+}
+
+static int xsk_configure(xsk_info* xsk, net_info* net, umem_info* umem)
+{
+    int ret = 0;
+    __u32 xdp_prog = -1;
+
+    if (umem == NULL) {
+        return EINVAL;
+    }
+
+    ret = xsk_umem__create(&umem->umem, umem->buffer, UMEM_LEN,
+        &umem->fill_ring, &umem->comp_ring, NULL);
+    if (ret) {
+        return ret;
+    }
+
+    struct xsk_socket_config xsk_config = {
+        .rx_size = XSK_RING_CONS__DEFAULT_NUM_DESCS,
+        .tx_size = XSK_RING_PROD__DEFAULT_NUM_DESCS,
+        .bind_flags = xsk->bind_flags,
+        .libbpf_flags = xsk->libbpf_flags,
+        .xdp_flags = xsk->xdp_flags
+    };
+
+    ret = xsk_socket__create(&xsk->socket, net->ifname, net->qid, umem->umem,
+        &xsk->rx, &xsk->tx, &xsk_config);
+    if (ret) {
+        return ret;
+    }
+
+    int ifindex = if_nametoindex(net->ifname);
+    ret = bpf_get_link_xdp_id(ifindex, &xdp_prog, xsk->xdp_flags);
+    if (ret) {
+        return ret;
+    }
+
+    printf("XDP Program ID: %d\n", xdp_prog);
+
+    // push all frames to fill ring
+    __u32 idx;
+    ret = xsk_ring_prod__reserve(&umem->fill_ring, XSK_RING_PROD__DEFAULT_NUM_DESCS, &idx);
+    if (ret != XSK_RING_PROD__DEFAULT_NUM_DESCS) {
+        return EIO;
+    }
+
+    // fill them with something
+    for (int i = 0; i < XSK_RING_PROD__DEFAULT_NUM_DESCS; i++) {
+        *xsk_ring_prod__fill_addr(&umem->fill_ring, idx++) = FRAME_INVALID;
+    }
+
+    xsk_ring_prod__submit(&umem->fill_ring, XSK_RING_PROD__DEFAULT_NUM_DESCS);
+
+    return 0;
+}
+
+int process_packet(xsk_info* xsk, umem_info* umem, const struct xdp_desc* desc)
+{
+    (void)xsk;
+    struct ethhdr* frame = (struct ethhdr*)xsk_umem__get_data(umem->buffer, desc->addr);
+    printf("[SRC MAC] %s - [DST MAC] %s\n", frame->h_source, frame->h_dest);
+    return 0;
+}
+
+void do_rx(xsk_info* xsk, umem_info* umem)
+{
+    __u32 idx, recvd;
+    recvd = xsk_ring_cons__peek(&xsk->rx, 1000, &idx);
+    if (!recvd) {
+        return;
+    }
+
+    for (__u32 i = 0; i < recvd; i++) {
+        const struct xdp_desc* desc = xsk_ring_cons__rx_desc(&xsk->rx, idx);
+        process_packet(xsk, umem, desc);
+        idx++;
+    }
+    xsk_ring_cons__release(&xsk->rx, recvd);
+}
+
+void process_rx(xsk_info* xsk, umem_info* umem)
+{
+    struct pollfd fds[2];
+    memset(fds, 0, sizeof(fds));
+
+    fds[0].fd = xsk_socket__fd(xsk->socket);
+    fds[0].events = POLLIN;
+    while (!break_flag) {
+        int ret = poll(fds, 1, 1000);
+        if (ret < 0) {
+            printf("[Poll-%d] %s\n", __LINE__, strerror(errno));
+        } else if (ret == 0) {
+            printf("[Poll] Timeout\n");
+        } else {
+            do_rx(xsk, umem);
+        }
+    }
 }
 
 int main(int argc, char** argv)
 {
-    struct daq_bpf* skel;
-    int err;
+    (void)argc;
+    (void)argv;
 
-    libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
-    /* Set up libbpf errors and debug info callback */
-    libbpf_set_print(libbpf_print_fn);
+    xsk_info xsk = {
+        .bind_flags = XDP_ZEROCOPY | XDP_USE_NEED_WAKEUP,
+        .libbpf_flags = 0,
+        .xdp_flags = XDP_FLAGS_DRV_MODE,
+    };
 
-    /* Open BPF application */
-    skel = daq_bpf__open();
-    if (!skel) {
-        fprintf(stderr, "Failed to open BPF skeleton\n");
-        return 1;
+    net_info net = {
+        .ifname = "",
+        .qid = 10
+    };
+
+    umem_info* umem = umem_info_create();
+    int ret = xsk_configure(&xsk, &net, umem);
+
+    if (ret) {
+        goto error;
     }
 
-    /* ensure BPF program only handles write() syscalls from our process */
-    skel->bss->my_pid = getpid();
-
-    /* Load & verify BPF programs */
-    err = daq_bpf__load(skel);
-    if (err) {
-        fprintf(stderr, "Failed to load and verify BPF skeleton\n");
-        goto cleanup;
-    }
-
-    /* Attach tracepoint handler */
-    err = daq_bpf__attach(skel);
-    if (err) {
-        fprintf(stderr, "Failed to attach BPF skeleton\n");
-        goto cleanup;
-    }
-
-    printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
-           "to see output of the BPF programs.\n");
-
-    for (;;) {
-        /* trigger our BPF program */
-        fprintf(stderr, ".");
-        sleep(1);
-    }
+    goto cleanup;
+error:
+    perror("Error: ");
+    printf("%d\n", ret);
 
 cleanup:
-    daq_bpf__destroy(skel);
-    return -err;
+    xsk_socket__delete(xsk.socket);
+    umem_info_free(umem);
 }
